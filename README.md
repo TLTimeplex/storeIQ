2e06a95efc5b308e242babab6b447b64962b16d5
Tim <72736935+TLTimeplex@users.noreply.github.com>
2024-02-26 14:01:35 +0100

    Adding dist to repo

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # storeIQ
- 
+Introducing a user-friendly JavaScript library designed for effortless data storage. This library simplifies the process by analyzing the data and determining the most efficient storage location. Moreover, it enhances performance by caching data in memory for quicker access during repeated use. With this tool, managing data becomes both straightforward and efficient.
--- tsup.config.ts
+++ tsup.config.ts
@@ -5,8 +5,15 @@ export default defineConfig({
   format: ["cjs", "esm"], // Build for commonJS and ESmodules
   dts: true, // Generate declaration file (.d.ts)
   splitting: false,
-  sourcemap: true,
+  sourcemap: false,
   clean: true,
-  minify: false, // Remember: Enable minify for production builds
-  target: ["es2020","chrome58","edge16","firefox57","node12","safari11"], // Emit modern ECMAScript
+  minify: true, // Remember: Enable minify for production builds
+  target: ["es2020", "chrome58", "edge16", "firefox57", "node12", "safari11"], // Emit modern ECMAScript
+  outExtension: () => {
+    return {
+      js: ".min.js",
+      dts: ".d.ts",
+    }
+  }
+  
 });
